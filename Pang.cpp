09999ea72888b7ec5890c9fc3786062b8d500b6f
nikitazhukov2911 <61113968+nikitazhukov2911@users.noreply.github.com>
2023-05-21 20:21:36 +0200

    Laboratorio_1
    Movimiento de la esfera, aumento de tamaño, cambio de color

--- Pang.cpp
+++ Pang.cpp
@@ -1,6 +1,43 @@
 #include "freeglut.h"
 
-//Hello
+
+ struct Esfera {
+	float radio= 2.0f;
+	float x = 0.0f;
+	float y = 0.0f;
+	float z= 0.0f;
+	unsigned char rojo= 255;
+	unsigned char verde= 255;
+	unsigned char azul= 255;
+};
+
+ void Dibuja(const Esfera& e)
+ {
+	 glColor3ub(e.rojo, e.verde, e.azul);
+	 glTranslatef(e.x, e.y, 0);
+	 glutSolidSphere(e.radio, 20, 20);
+	 glTranslatef(-e.x, -e.y, 0);
+ }
+
+ void Mueve(Esfera* e)
+ {
+	 e->radio += 0.1f;
+	 if (e->radio > 3)
+	 {
+		 e->radio = 0.5f;
+
+	 }
+	
+ }
+
+ float var_x = 0.0f,var_y;
+
+ 
+
+
+
+ Esfera esfera = { 2.0f,0.0f,0.0f,0.0f,255,255,255 };
+ Esfera esfera2 = { 2.0f,0.0f,0.0f,0.0f,255,255,255 };
 
 //los callback, funciones que seran llamadas automaticamente por la glut
 //cuando sucedan eventos
@@ -45,23 +82,75 @@ void OnDraw(void)
 	glMatrixMode(GL_MODELVIEW);	
 	glLoadIdentity();
 	
-	gluLookAt(0.0, 10, 20,  // posicion del ojo
+	gluLookAt(var_x, 10, 20,  // posicion del ojo
 		0.0, 0, 0.0,      // hacia que punto mira  (0,0,0) 
 		0.0, 1.0, 0.0);      // definimos hacia arriba (eje Y)    
 
 	//aqui es donde hay que poner el código de dibujo
-	glutWireCube(5);
+	Dibuja(esfera);
+	Dibuja(esfera2);
+	glBegin(GL_POLYGON);
+	glColor3ub(255, 0, 0);
+	glVertex3f(-5.0f, -5.0f, 0.0f);
+	glColor3ub(255, 255, 0);
+	glVertex3f(-5.0f, 5.0f, 0.0f);
+	glColor3ub(255, 255, 255);
+	glVertex3f(5.0f, 5.0f, 0.0f);
+	glColor3ub(0, 255, 255);
+	glVertex3f(5.0f, -5.0f, 0.0f);
+	glEnd();
+
+	/*glTranslatef(-x_esfera, 0, 0);*/
 
 	//no borrar esta linea ni poner nada despues
 	glutSwapBuffers();
 }
 void OnKeyboardDown(unsigned char key, int x_t, int y_t)
 {
+	if (key == '1' && esfera.radio< 3) { esfera.radio+= 0.1f; }
+	if (key == '2' && esfera.radio> 1) { esfera.radio-= 0.1f; }
+	if (key == 'r')
+	{
+		esfera.rojo= 255;
+		esfera.verde= 0;
+		esfera.azul= 0;
+	}
+	if (key == 'g')
+	{
+		esfera.rojo= 0;
+		esfera.verde= 255;
+		esfera.azul= 0;
+	}
+	if (key == 'b')
+	{
+		esfera.rojo= 0;
+		esfera.verde= 0;
+		esfera.azul= 255;
+	}
+	if (key == 'a')//izq, X negativo
+		esfera.x-= 0.1f;
+	if (key == 'd')//izq, X negativo
+		esfera.x+= 0.1f;
+	if (key == 'w')//izq, X negativo
+		esfera.y+= 0.1f;
+	if (key == 's')//izq, X negativo
+		esfera.y-= 0.1f;
+	if (key == 'j')//izq, X negativo
+		var_x += 0.1f;
+	var_y += 0.1f;
+	glutPostRedisplay();
 
 }
 
 void OnTimer(int value)
 {
-	
+
+	Mueve(&esfera);
+	Mueve(&esfera2);
+	var_x += 0.1f;
+	var_y += 0.1f;
+	glutTimerFunc(25, OnTimer, 0);
+	glutPostRedisplay();
+
 }
 
